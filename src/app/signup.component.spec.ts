6c3455a0f830a9ee57d641fd9b7586a93a856402
mission2earth <mission2earth@gmail.com>
2018-08-23 17:24:00 +0530

    last commit

--- src/app/signup.component.spec.ts
+++ src/app/signup.component.spec.ts
@@ -2,16 +2,27 @@ import { TestBed } from "@angular/core/testing";
 import { RegisterComponent } from "./login/register.component";
 import { MySignComponent } from "./shared/mysign.component";
 import { ReactiveFormsModule } from "@angular/forms";
+import { SignInComponent } from "./login/login.component";
 
 describe("Signup Test Demo", ()=> 
 {
-it("should verify instance of Register component", ()=> {
-    TestBed.configureTestingModule({
-        declarations: [RegisterComponent, MySignComponent],
-        imports: [ReactiveFormsModule]
+    beforeEach(()=> {
+        TestBed.configureTestingModule({
+            declarations: [RegisterComponent, MySignComponent],
+            imports: [ReactiveFormsModule]
+        })
     })
+it("should verify instance of Register component", ()=> {    
     let f = TestBed.createComponent(RegisterComponent)
     expect(f).toBeDefined();
+})
+
+it("should verify Datamember", ()=>
+{
+let f = TestBed.createComponent(RegisterComponent);
+let obj = f.componentInstance;
+
+expect(obj.registerHeading).toEqual("Register")
 })
 
 })
